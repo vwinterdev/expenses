bb85a4b93e1b51161a266e44afecbea55be57a93
Vladislav Akulinin <vwinterdev@gmail.com>
2026-03-24 12:56:59 +0300

    save

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [vue(), VitePWA({
-    registerType: 'autoUpdate'
+    registerType: 'autoUpdate',
+    manifest: {
+      theme_color: '#ffffff',
+    },
   })],
   resolve: {
     alias: {
